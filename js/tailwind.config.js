b7e3d5d940e56a665fdd10a4a3f366c36cca08cd
Mahiuha <tiasho.mahiuha@gmail.com>
2023-08-21 01:52:45 +0300

    add semantic html layout and assets

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        figmaorange: "#F21919",
+        figmablue: "#3357F8",
+        figmastone: "#E0E0E0",
+        figmasky: "#F2F5FF",
+      },
+      fontFamily: {
+        display: ["poppins"],
+        body: ["montserrat"],
+      },
+    },
+  },
+};
