4c64fcff2062224a34f282e099ef5a6771dae8a4
Mahiuha <tiasho.mahiuha@gmail.com>
2023-08-26 23:42:15 +0300

    add mobile view

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -6,6 +6,9 @@ tailwind.config = {
         figmablue: "#3357F8",
         figmastone: "#E0E0E0",
         figmasky: "#F2F5FF",
+        figmablack: "#000000",
+        figmagray: "#A8A8A8",
+        figmateal: "#1093B5",
       },
       fontFamily: {
         display: ["poppins"],
